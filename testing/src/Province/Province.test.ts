3d64f991685e075db64d60b7aa28a063c7456cd6
methinks-vini <vini@methinks.io>
2022-12-26 17:41:02 +0900

    1. ignore dist js files 2. added run scripts in package.json 3. added failed situation shortfall

--- testing/src/Province/Province.test.ts
+++ testing/src/Province/Province.test.ts
@@ -2,8 +2,8 @@ import Province from './Province';
 import { sampleProvinceData } from '../samples/samples';
 
 describe('Province', () => {
-  it('shortfall', () => {
+  it('get shortfall', () => {
     const asia = new Province(sampleProvinceData()); // 1 픽스처 설정
-    expect(asia.shortfall).toBe(5); //2 검증
+    expect(asia.shortfall).toEqual(5); //2 검증
   });
 });
--- testing/src/Province/Province.ts
+++ testing/src/Province/Province.ts
@@ -59,7 +59,7 @@ export default class Province {
   }
 
   get shortfall() {
-    return this._demand - this._totalProduction;
+    return this._demand - this._totalProduction * 2;
   }
 
   get profit() {
