aa43b4eba91a5a43dafbecc6971290807866113e
methinks-vini <vini@methinks.io>
2022-12-26 18:46:38 +0900

    Cannot find module @jest/globals issue fixed

--- testing/jest.config.js
+++ testing/jest.config.js
@@ -1,6 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
+  roots: ['<rootDir>'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['./dist/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
 };
