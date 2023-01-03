e1702e4c5c558849a3f68fd990570547c212d031
methinks-vini <vini@methinks.io>
2023-01-03 21:42:55 +0900

    6.1 method extract ex2 st.2

--- chapter6/Extract_function/exampe_using_inner_scope_vars.js
+++ chapter6/Extract_function/exampe_using_inner_scope_vars.js
@@ -15,15 +15,23 @@ function printOwing(invoice) {
     outstanding += o.amount;
   }
 
+  recordDueDate(invoice);
   //마감일(dueDate)을 기록한다.
-  const today = Clock.today;
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
+  printDetails(invoice, outstanding); //지역 변수를 매개 변수로 전달
+}
+
+function printBanner() {
+  console.log('***** 고객 채무 ******');
+}
+
+function printDetails(invoice, outstanding) {
   console.log(` 고객명: ${invoice.customer}`);
   console.log(` 채무액: ${outstanding}`);
   console.log(` 마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
 
-function printBanner() {
-  console.log('***** 고객 채무 ******');
+function recordDueDate(invoice) {
+  const today = Clock.today;
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
